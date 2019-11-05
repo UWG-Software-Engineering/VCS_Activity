7b5583cccec1a94cea90666ebf83f3b634b7a992
Darrius Dupree <csuser@160.10.217.126>
2019-11-05 17:17:17 -0500

    Adds object to Main:main

--- VCS Activity/src/edu/westga/cs3211/vcs_activity/Main.java
+++ VCS Activity/src/edu/westga/cs3211/vcs_activity/Main.java
@@ -1,6 +1,7 @@
 package edu.westga.cs3211.vcs_activity;
 
 
+import edu.westga.cs3211.vcs_activity.model.Ice_Cream;
 import edu.westga.cs3211.vcs_activity.model.Student;
 
 /** 
@@ -25,6 +26,12 @@ public class Main {
 		System.out.println("The two students are equal: " + student.equals(otherStudent));
 
 		// Thomas Whaley end
+		
+		//Darrius Dupree Start
+		Ice_Cream iceCream = new Ice_Cream("Chocolate", true);
+		Ice_Cream iceCream2 = new Ice_Cream("Chocolate", true);
+		System.out.println("The stuff is not equal " + iceCream2.equals(iceCream));
+	
 	}
 
 }
