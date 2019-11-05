ff3bb550194b331f40924febf6138c6f3d93d0cb
Wilson <Wils0248n@gmail.com>
2019-11-05 17:12:51 -0500

    Added Tests for Donut equals method

---
+++ VCS Activity/src/edu/westga/cs3211/vcs_activity/test/donut/DonutEqualsTest.java
@@ -0,0 +1,45 @@
+package edu.westga.cs3211.vcs_activity.test.donut;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3211.vcs_activity.model.Donut;
+
+public class DonutEqualsTest {
+
+	@Test
+	void testSameDonut() {
+		Donut aDonut = new Donut(3, true);
+		assertTrue(aDonut.equals(aDonut));
+	}
+	
+	@Test
+	void testDifferentDonutSameParameters() {
+		Donut aDonut = new Donut(3, true);
+		Donut bDonut = new Donut(3, true);
+		assertTrue(aDonut.equals(bDonut));
+	}
+	
+	@Test
+	void testDifferentCaloriesSameSprinkles() {
+		Donut aDonut = new Donut(3, true);
+		Donut bDonut = new Donut(2, true);
+		assertFalse(aDonut.equals(bDonut));
+	}
+	
+	@Test
+	void testDifferentSprinklesSameCalories() {
+		Donut aDonut = new Donut(3, true);
+		Donut bDonut = new Donut(3, false);
+		assertFalse(aDonut.equals(bDonut));
+	}
+	
+	@Test
+	void testDifferentDonuts() {
+		Donut aDonut = new Donut(3, true);
+		Donut bDonut = new Donut(2, false);
+		assertFalse(aDonut.equals(bDonut));
+	}
+}
