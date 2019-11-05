85b33a104bbdf2418964c6065f0f3ed1b85886ed
Jonathan Corley <Jonathan Corley@160.10.25.78>
2019-11-05 10:51:35 -0500

    Adding Skeleton for Activity

---
+++ VCS Activity/src/edu/westga/cs3211/vcs_activity/Main.java
@@ -0,0 +1,18 @@
+package edu.westga.cs3211.vcs_activity;
+
+/** Contains the entry point for the program.
+ * 
+ * @author Jonathan Corley
+ * @version Fall 2019
+ */
+public class Main {
+	
+	/** Entry point for the program.
+	 * 
+	 * @param args command line arguments (we won't need this!)
+	 */
+	public static void main(String[] args) {
+		System.out.println("Jonathan Corley added this!");
+	}
+
+}
