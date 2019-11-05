a1a35df70bc9ed927668bc4c6e8cca424ba49fc4
Justin Cintron <csuser@160.10.217.73>
2019-11-05 16:43:15 -0500

    Added toString method to convert and format page number.

--- VCS Activity/src/edu/westga/cs3211/vcs_activity/model/Book.java
+++ VCS Activity/src/edu/westga/cs3211/vcs_activity/model/Book.java
@@ -1,8 +1,17 @@
 package edu.westga.cs3211.vcs_activity.model;
-
+/**
+ * 
+ * @author Justin Cintron
+ *
+ */
 public class Book {
 
 	int pageCount;
 	String title;
+	
+	public String toString(int pageCount) {
+		String pageFormated = "Page : " + pageCount;
+		return pageFormated;
+	}
 }
 
