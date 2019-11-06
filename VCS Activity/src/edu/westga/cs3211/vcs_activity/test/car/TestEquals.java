9c87579ae19143ead5988ead963e038cc840e250
Michael Pavich <mpavich2@my.westga.edu>
2019-11-06 17:41:26 -0500

    Added test cases for equals method for car

--- VCS Activity/src/edu/westga/cs3211/vcs_activity/test/car/TestEquals.java
+++ VCS Activity/src/edu/westga/cs3211/vcs_activity/test/car/TestEquals.java
@@ -4,11 +4,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs3211.vcs_activity.model.Car;
+
 class TestEquals {
 
 	@Test
-	void test() {
-		
+	void testEqualsValid() {
+		Car car = new Car(100, "Red");
+		Car other = new Car(100, "Red");
+		assertEquals(true, car.equals(other));
 	}
+	
+	@Test
+	void testOnlySpeedEqual() {
+		Car car = new Car(100, "Yellow");
+		Car other = new Car(100, "Red");
+		assertEquals(false, car.equals(other));
+	}
+	
+	@Test
+	void testOnlyColorEqual() {
+		Car car = new Car(50, "Red");
+		Car other = new Car(100, "Red");
+		assertEquals(false, car.equals(other));
+	}
+	
+	@Test
+	void testNothingEqual() {
+		Car car = new Car(50, "Yellow");
+		Car other = new Car(100, "Red");
+		assertEquals(false, car.equals(other));
+	}
+	
 
 }
