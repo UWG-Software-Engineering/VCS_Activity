bf631bf6663fc9e9bcf333589b7d593c246ccea5
Daniel Crumpler <dancrumpy@gmail.com>
2020-11-10 13:05:19 -0500

    Added boat class

---
+++ VCS Activity/src/model/Boat.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Boat {
+
+	private String brand;
+	private int length;
+	
+	public Boat (String brand, int length) {
+		this.brand = brand;
+		this.length = length;
+	}
+}
