4036905a0735e4bbb646318d90764cee2bec16ab
Austin Snyder <csuser@160.10.217.72>
2020-11-12 13:18:13 -0500

    Made String uppercase and null use == not .equals

--- VCS Activity/src/model/Desk.java
+++ VCS Activity/src/model/Desk.java
@@ -5,12 +5,12 @@
  */
 public class Desk {
 
-	private string woodType;
+	private String woodType;
 	private int numberOfDrawers;
 	
 	public Desk(String woodType, int numberOfDrawers) {
-		if (woodType.equals(null)) {
-			throw new IllegalArgumentException("Wood type musrt bot be null");
+		if (woodType == null) {
+			throw new IllegalArgumentException("Wood type must not be null");
 		}
 		if (numberOfDrawers <= 0) {
 			throw new IllegalArgumentException("Number of drawers must be greater than or equal to 0");
