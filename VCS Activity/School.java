ed3ff8dfa4d81d2e575996f5b329f2ad40887420
csuser <csuser@160.10.217.89>
2020-11-12 13:04:03 -0500

    Initial new object

---
+++ VCS Activity/School.java
@@ -0,0 +1,19 @@
+
+public class School {
+	 
+	private String name;
+	private String location;
+	
+	/**
+	 * Initial new school
+	 * @param name The name of the school
+	 * @param location
+	 */
+	public School(String name, String location) {
+		this.name = name;
+		this.location = location;
+	}
+	
+	
+
+}
