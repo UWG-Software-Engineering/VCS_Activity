6abc89319b5d2f6124485834fd3168ee1c3f9a32
csuser <csuser@160.10.217.89>
2020-11-12 13:16:24 -0500

    Create school object

--- VCS Activity/src/model/School.java
+++ VCS Activity/src/model/School.java
@@ -6,14 +6,35 @@ public class School {
 	
 	/**
 	 * Initial new school
+	 * precondition name != null && 
 	 * @param name The name of the school
-	 * @param location
+	 * @param location The location of the School
 	 */
 	public School(String name, String location) {
+		if(name == null) {
+			throw new IllegalArgumentException("Name cannot be null.");
+		}
+		if(location == null) {
+			throw new IllegalArgumentException("location cannot be null.");
+		}
 		this.name = name;
 		this.location = location;
 	}
 	
+	/**
+	 * Get the shcool name
+	 */
+	public void getName() {
+		return this.name;
+
+	}
+	
+	/**
+	 * Get the location
+	 */
+	public void getLocation() {
+		return this.location;
+	}
 	
 
 }
